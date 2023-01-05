5530e7e36ab5d56602f8704480b573e47c08f6d6
Eduard Minguell <60965207+Eduardiko@users.noreply.github.com>
2023-01-05 18:33:18 +0100

    Temp Push: Lobby Bombs

--- Assets/Scripts/SceneScripts/BackgroundBehaviour.cs
+++ Assets/Scripts/SceneScripts/BackgroundBehaviour.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class BackgroundBehaviour : MonoBehaviour
 {
     private Rigidbody2D rb;
     private RectTransform rtransform;
 
-    private float addForceTime = 0.3f;
+    public float timerTime = 0f;
 
     private Vector2 initPos;
+    private Vector2 currentPos;
 
     public float forceMin;
     public float forceMax;
@@ -24,84 +26,162 @@ public class BackgroundBehaviour : MonoBehaviour
 
     public float mass;
 
-    private bool explosionOnce = false;
+    public float horizontalSpeed;
+
+    private bool triggerOnce = false;
 
     private CameraShaker shaker;
 
     public Sprite normalBomb;
     public Sprite explodedBomb;
 
+    public GameObject backgroundBomb;
+
     private Image image;
+
+    public enum MovementType
+    {
+        RANDOM,
+        HORIZONTAL,
+        VERTICAL
+    }
+
+    public MovementType movementType;
+
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         rtransform = GetComponent<RectTransform>();
         image = GetComponent<Image>();
-        shaker = GameObject.Find("Shaker").GetComponent<CameraShaker>();
+
+        if (movementType == MovementType.RANDOM)
+        {
+            rb = GetComponent<Rigidbody2D>();
+            rb.mass = mass;
+            shaker = GameObject.Find("Shaker").GetComponent<CameraShaker>();
+        }
+        else
+        {
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(this.GetComponent<Rigidbody2D>());
+        }
 
 
-        initPos = rtransform.localPosition;
-
-        rb.mass = mass;
-
-        Vector2 direction = new Vector2((float)Random.Range(transform.position.x - 180, transform.position.x + 180), (float)Random.Range(transform.position.y - 180, transform.position.y + 180));
-        float force = (float)Random.Range(forceMin, forceMax);
-        rb.AddForce(direction * force);
-
-
-        rotSpeed = (float)Random.Range(rotSpeedMin, rotSpeedMax);
-
-        rb.AddTorque(force * rotSpeed);
+        initPos = rtransform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (shaker.active)
+        switch (movementType)
         {
-            explosionOnce = true;
-            image.sprite = explodedBomb;
+            case MovementType.RANDOM:
+
+                if (shaker.active)
+                {
+                    triggerOnce = true;
+                    image.sprite = explodedBomb;
+                }
+                else
+                {
+                    if (triggerOnce)
+                    {
+                        rtransform.position = initPos;
+                        rb.velocity = Vector2.zero;
+                        rb.angularVelocity = 0f;
+                        triggerOnce = false;
+                    }
+
+                    image.sprite = normalBomb;
+                }
+
+
+                if (timerTime > 0f)
+                {
+                    timerTime -= Time.deltaTime;
+
+                }
+                else
+                {
+                    Vector2 direction = new Vector2((float)Random.Range(transform.position.x - 180, transform.position.x + 180), (float)Random.Range(transform.position.y, transform.position.y + 180));
+                    float force = (float)Random.Range(forceMin, forceMax);
+                    rb.AddForce(direction * force);
+
+                    rb.angularVelocity = 0f;
+
+                    rotSpeed = (float)Random.Range(rotSpeedMin, rotSpeedMax);
+
+                    rb.AddTorque(force * rotSpeed);
+
+
+                    timerTime = Random.Range(timeForceMin, timeForceMax);
+                }
+                break;
+            case MovementType.HORIZONTAL:
+
+                if (timerTime > 0)
+                    timerTime -= Time.deltaTime;
+                else
+                { 
+                    Destroy(this.gameObject);
+                    return;
+                }
+
+                if (Mathf.Abs(currentPos.x - initPos.x) > 148)
+                {
+                    if (!triggerOnce)
+                    {
+                        triggerOnce = true;
+                  
+                        GameObject temp = Instantiate(backgroundBomb);
+                        temp.name = temp.name + Random.Range(0f, 255.5f).ToString();
+                        temp.transform.SetParent(gameObject.transform.parent);
+
+                        temp.GetComponent<RectTransform>().position = currentPos;
+                        temp.GetComponent<RectTransform>().position -= new Vector3(160, 0, 0);
+                        temp.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+                        temp.GetComponent<RectTransform>().rotation = new Quaternion();
+                        temp.GetComponent<BackgroundBehaviour>().initPos = temp.GetComponent<RectTransform>().position;
+                        temp.GetComponent<BackgroundBehaviour>().movementType = MovementType.HORIZONTAL;
+                        temp.GetComponent<BackgroundBehaviour>().timerTime = 10f;
+                    }
+                }
+
+                currentPos.x = rtransform.position.x + horizontalSpeed * Time.deltaTime;
+                currentPos.y = rtransform.position.y;
+
+                rtransform.position = currentPos;
+                break;
+            case MovementType.VERTICAL:
+                initPos.y = rtransform.position.y + horizontalSpeed * Time.deltaTime;
+                initPos.x = rtransform.position.x;
+
+                rtransform.position = initPos;
+                break;
+            default:
+                break;
         }
-        else
-        {
-            if(explosionOnce)
-            {
-                rtransform.localPosition = initPos;
-                rb.velocity = Vector2.zero;
-                rb.angularVelocity = 0f;
-                explosionOnce = false;
-            }
 
-            image.sprite = normalBomb;
-        }
-
-        if(addForceTime > 0f)
-        {
-            addForceTime -= Time.deltaTime;
-
-        } else
-        {
-            Vector2 direction = new Vector2((float)Random.Range(transform.position.x - 180, transform.position.x + 180), (float)Random.Range(transform.position.y, transform.position.y + 180));
-            float force = (float)Random.Range(forceMin, forceMax);
-            rb.AddForce(direction * force);
-
-            rb.angularVelocity = 0f;
-
-            rotSpeed = (float)Random.Range(rotSpeedMin, rotSpeedMax);
-
-            rb.AddTorque(force * rotSpeed);
-
-
-            addForceTime = Random.Range(timeForceMin, timeForceMax);
-        }
     }
 
     private void OnBecameInvisible()
     {
-        rtransform.localPosition = initPos;
-        rb.velocity = Vector2.zero;
-        rb.angularVelocity = 0f;
+        switch (movementType)
+        {
+            case MovementType.RANDOM:
+                rtransform.localPosition = initPos;
+                rb.velocity = Vector2.zero;
+                rb.angularVelocity = 0f;
+                break;
+            case MovementType.HORIZONTAL:
+                Destroy(gameObject);
+                break;
+            case MovementType.VERTICAL:
+                Destroy(gameObject);
+                break;
+            default:
+                break;
+        }
     }
 }
